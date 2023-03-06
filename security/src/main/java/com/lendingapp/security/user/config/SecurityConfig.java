8680499310724bf751ffb6626a6cb450f22600e9
Ramghimirecr7 <ghimireram4@gmail.com>
2023-03-05 20:07:38 -0600

    removed security project

--- security/src/main/java/com/lendingapp/security/user/config/SecurityConfig.java
+++
@@ -1,8 +0,0 @@
-package com.lendingapp.security.user.config;
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
-
-public class SecurityConfig {
-
-}
